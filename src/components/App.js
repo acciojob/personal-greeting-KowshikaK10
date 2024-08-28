6a47750a41d0a8904506d7cfb3cb2fc5f7935c31
KowshikaK10 <kowshi010101@gmail.com>
2024-08-28 18:03:31 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[name,setName]=useState('');
   return (
     <div>
         {/* Do not remove the main div */}
+        <label htmlFor='name'>Enter your name :</label><br /><br />
+        <input onChange={(e)=>setName(e.target.value)} type="text" />
+        {name && <p>Hello {name}!</p>}
     </div>
   )
 }
